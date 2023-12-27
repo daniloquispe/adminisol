6d400037e23a7a32ad0d4251374a22c8f83ce5cf
Danilo Quispe <dql@daniloquispe.dev>
2023-12-27 13:14:24 -0500

    Update users seeder
    Now, default user will be created as "verified", so login action won't be affected by security policy.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,6 +18,6 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-		User::create(['name' => 'Danilo', 'email' => 'dql@daniloquispe.dev', 'password' => '123456']);
+		User::create(['name' => 'Danilo', 'email' => 'dql@daniloquispe.dev', 'password' => '123456', 'email_verified_at' => now()]);
     }
 }
