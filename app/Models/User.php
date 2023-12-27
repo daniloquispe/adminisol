4a8b15a787032b7586019b38d9571f9c9823ffa6
Danilo Quispe <dql@daniloquispe.dev>
2023-12-26 21:43:56 -0500

    Allowing users to access a Filament panel.
    By default, all {@see User} models can access Filament locally. However, when deploying to production, you must update your `App\Models\User.php` to implement the FilamentUser contract — ensuring that only the correct users can access your panel.
    
    Reference: https://filamentphp.com/docs/3.x/panels/installation#deploying-to-production

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,17 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+/**
+ * @property string $email
+ */
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -42,4 +47,18 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+	/**
+	 * Allowing users to access a Filament panel.
+	 *
+	 * By default, all {@see User} models can access Filament locally. However, when deploying to production, you must
+	 * update your `App\Models\User.php` to implement the FilamentUser contract — ensuring that only the correct users
+	 * can access your panel.
+	 *
+	 * @link https://filamentphp.com/docs/3.x/panels/installation#deploying-to-production Filament documentation
+	 */
+	public function canAccessPanel(Panel $panel): bool
+	{
+		return str_ends_with($this->email, '@daniloquispe.dev') && $this->hasVerifiedEmail();
+	}
 }
