14a73b1b8f621afde9c440380f68a06d07096cc8
Danilo Quispe <dql@daniloquispe.dev>
2025-03-12 00:11:15 -0500

    Update PHP code for Laravel 12

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,6 @@ use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 /**
  * Model for a system user.
@@ -19,7 +18,7 @@ use Laravel\Sanctum\HasApiTokens;
  */
 class User extends Authenticatable implements FilamentUser
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
